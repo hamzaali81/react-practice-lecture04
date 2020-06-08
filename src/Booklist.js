eeb41ce4c8c38e11fbd9c966ebadc846300701ed
hamzaali <hamza54834@gmail.com>
2020-06-08 08:49:00 -0700

    state and alternative state syntax

--- src/Booklist.js
+++ src/Booklist.js
@@ -1,21 +1,54 @@
+// State, this.setState= hold/access/change data of component
+// props = read Only 
+
+
 import React, { Component } from 'react';
 import Book from './Book';
+import booksData from './bookData';
 
 class Booklist extends Component {
- state={
-    books:[
-        {
-            id: 1,
-            book:"book number one",
-            author:"ahmed"
-        },
-        {
-            id:2,
-            book:"book number two",
-            author:"ali"
+    constructor(props){
+        super(props)
+        this.state={
+            
+                books: booksData
         }
-    ]
- };
+    }
+//  state={
+//     books:[
+//         {
+//             id: 1,
+//             book:"book number one",
+//             author:"ahmed"
+//         },
+//         {
+//             id:2,
+//             book:"book number two",
+//             author:"ali"
+//         }
+//     ]
+//  };
+ 
+
+    // state={
+    //  books: booksData
+
+    //  [
+        //  {  
+        //     //   id: 1,
+        //      name: "hamza ali",
+        //      job: "designer"
+        //  },
+        //    {
+        //     //    id: 2,
+        //        name:"ahmed",
+        //        job:"developer"
+        // }
+
+    //  ]
+    // }
+
+
     render() {
         //this.setState({})
         // const books=this.state.books.map((item)=>item.book);
@@ -25,13 +58,16 @@ class Booklist extends Component {
         //forEach
         //map
         //reduce
+
+        
+
         return (
             <section>
                 <h3>This is our Booklist</h3>
                 {/* <Book book={this.books[0]}/> */}
                 {/* {this.state.books.map((item,index)=> (<Book key={index} info={item} />))} */}
-                {this.state.books.map((item)=> (<Book key={item.id} info={item} />))}
-
+                {/* {this.state.books.map((item)=> (<Book key={item.id} info={item} />))} */}
+                 {this.state.books.map((item,index)=>(<Book key={index} info={item} />))}
             </section>
         );
     }
---
+++ src/bookData.js
@@ -0,0 +1,30 @@
+const books=[
+    {  
+          id: 1,
+         name: "hamza ali",
+         job: "designer"
+     },
+       {
+           id: 2,
+           name:"ahmed",
+           job:"developer"
+    }
+    ,
+       {
+           id: 3,
+           name:"mustafa",
+           job:"shopping"
+    }
+    ,
+       {
+           id: 4,
+           name:"umer",
+           job:"boss"
+    }
+    ,
+       {
+           id: 5,
+           name:"usman",
+           job:"programmer"
+    }
+]
