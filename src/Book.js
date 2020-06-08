dbacb6287f3e898589fbd0f92b560fea4ac9723e
hamzaali <hamza54834@gmail.com>
2020-06-08 12:48:30 -0700

    Events in react

--- src/Book.js
+++ src/Book.js
@@ -3,6 +3,25 @@ import React, { Component } from 'react';
 // import React from 'react';
 
 class Book extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            count: 1 
+        }
+        // this.handleClick = this.handleClick.bind(this);
+    }
+
+
+
+//    handleClick(){
+//        console.log(this.state.count)
+//    }
+
+handleClick=()=> {
+    console.log("You clicked me");
+    console.log(this.state.count)
+}
+
     render() {
         // console.log(this.props)
         // const {book,author}=this.props.book;
@@ -17,6 +36,7 @@ class Book extends Component {
 
                 <h3>Author: {author}</h3>
                 <h5>Title :{title}</h5>
+                <button type="button" onClick={this.handleClick}>Add Count</button>
             </div>
             </article>
         );
